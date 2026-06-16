7838aba0cfb357fa1fc1d5b922da11a5e6eb9eb9
Khang <khangnguyen41888@gmail.com>
2026-06-16 13:50:48 +0700

    add cicd api endpoint to test workflow

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -28,6 +28,13 @@ def week4():
 @app.route('/health')
 def health():
     return {'status': 'healthy', 'message': 'Server is running'}
+
+@app.route('/cicd')
+def cicd():
+    return jsonify({
+        "message": "Hello from CI/CD!",
+        "status": "ok"
+    })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
